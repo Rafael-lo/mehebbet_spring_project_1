115016fc0557332015b3f63bd76723f97a296fee
Rafael-lo <72205368+Rafael-lo@users.noreply.github.com>
2024-11-26 21:51:48 +0400

    Transfered models from old project

--- mehebbet_spring_project_1/src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/models/Basket.java
+++ mehebbet_spring_project_1/src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/models/Basket.java
@@ -1,5 +1,21 @@
 package com.mehebbet_spring_project_1.mehebbet_spring_project_1.models;
 
-public class Basket {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "baskets")
+public class Basket {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // User who owns the basket
 }
