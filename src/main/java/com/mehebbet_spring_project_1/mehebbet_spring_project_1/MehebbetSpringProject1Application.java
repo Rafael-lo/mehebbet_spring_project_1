25b194124f671125abf57c56a8f356d7cad6d77d
Rafael-lo <72205368+Rafael-lo@users.noreply.github.com>
2024-12-11 17:13:22 +0400

    start new project

---
+++ src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/MehebbetSpringProject1Application.java
@@ -0,0 +1,13 @@
+package com.mehebbet_spring_project_1.mehebbet_spring_project_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehebbetSpringProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehebbetSpringProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/MehebbetSpringProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehebbet_spring_project_1.mehebbet_spring_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MehebbetSpringProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
