9e64aa76bd522f83e03c9a99dcdc3d97a3aafecd
Rafael-lo <me.farzaliyev@gmail.com>
2025-01-17 20:30:49 +0400

    feat(html): added html files and folders

--- src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/services/CartService.java
+++ src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/services/CartService.java
@@ -1,5 +1,15 @@
 package com.mehebbet_spring_project_1.mehebbet_spring_project_1.services;
 
-public interface CartService {
+import java.util.List;
 
+import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.Cart;
+
+public interface CartService {
+	public Cart saveCart(Integer productId, Integer userId);
+
+	public List<Cart> getCartsByUser(Integer userId);
+	
+	public Integer getCountCart(Integer userId);
+
+	public void updateQuantity(String sy, Integer cid);
 }
--- src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/services/impl/OrderServiceImpl.java
+++ src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/services/impl/OrderServiceImpl.java
@@ -1,5 +1,49 @@
 package com.mehebbet_spring_project_1.mehebbet_spring_project_1.services.impl;
 
-public class OrderServiceImpl {
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.OrderRequest;
+import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.ProductOrder;
+import com.mehebbet_spring_project_1.mehebbet_spring_project_1.services.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public void saveOrder(Integer userid, OrderRequest orderRequest) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ProductOrder> getOrdersByUser(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductOrder updateOrderStatus(Integer id, String status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductOrder> getAllOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductOrder getOrdersByOrderId(String orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<ProductOrder> getAllOrdersPagination(Integer pageNo, Integer pageSize) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
