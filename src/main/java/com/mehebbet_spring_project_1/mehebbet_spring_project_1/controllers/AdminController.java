3cd451a4462c64372911563fc905d4701523412e
Rafael-lo <me.farzaliyev@gmail.com>
2025-01-31 21:46:41 +0400

    last step

--- src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/controllers/AdminController.java
+++ src/main/java/com/mehebbet_spring_project_1/mehebbet_spring_project_1/controllers/AdminController.java
@@ -3,8 +3,10 @@ package com.mehebbet_spring_project_1.mehebbet_spring_project_1.controllers;
 import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.Category;
 import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.Product;
 import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.ProductOrder;
+import com.mehebbet_spring_project_1.mehebbet_spring_project_1.models.UserDtls;
 import com.mehebbet_spring_project_1.mehebbet_spring_project_1.services.*;
 import com.mehebbet_spring_project_1.mehebbet_spring_project_1.util.CommonUtil;
+import com.mehebbet_spring_project_1.mehebbet_spring_project_1.util.OrderStatus;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -22,6 +24,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -268,5 +271,138 @@ public class AdminController {
         m.addAttribute("isLast", page.isLast());
 
         return "/admin/orders";
+    }
+
+    @PostMapping("/update-order-status")
+    public String updateOrderStatus(@RequestParam Integer id, @RequestParam Integer st, HttpSession session) {
+
+        OrderStatus[] values = OrderStatus.values();
+        String status = null;
+
+        for (OrderStatus orderSt : values) {
+            if (orderSt.getId().equals(st)) {
+                status = orderSt.getName();
+            }
+        }
+
+        ProductOrder updateOrder = orderService.updateOrderStatus(id, status);
+
+        try {
+            //commonUtil.sendMailForProductOrder(updateOrder, status);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (!ObjectUtils.isEmpty(updateOrder)) {
+            session.setAttribute("succMsg", "Статус изменен");
+        } else {
+            session.setAttribute("errorMsg", "Не удалось изменить статус");
+        }
+        return "redirect:/admin/orders";
+    }
+
+    @GetMapping("/search-order")
+    public String searchProduct(@RequestParam String orderId, Model m, HttpSession session,
+                                @RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
+                                @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+
+        if (orderId != null && orderId.length() > 0) {
+
+            ProductOrder order = orderService.getOrdersByOrderId(orderId.trim());
+
+            if (ObjectUtils.isEmpty(order)) {
+                session.setAttribute("errorMsg", "Неправильный номер заказа");
+                m.addAttribute("orderDtls", null);
+            } else {
+                m.addAttribute("orderDtls", order);
+            }
+
+            m.addAttribute("srch", true);
+        } else {
+
+            Page<ProductOrder> page = orderService.getAllOrdersPagination(pageNo, pageSize);
+            m.addAttribute("orders", page);
+            m.addAttribute("srch", false);
+
+            m.addAttribute("pageNo", page.getNumber());
+            m.addAttribute("pageSize", pageSize);
+            m.addAttribute("totalElements", page.getTotalElements());
+            m.addAttribute("totalPages", page.getTotalPages());
+            m.addAttribute("isFirst", page.isFirst());
+            m.addAttribute("isLast", page.isLast());
+
+        }
+        return "/admin/orders";
+
+    }
+
+    @GetMapping("/add-admin")
+    public String loadAdminAdd() {
+        return "/admin/add_admin";
+    }
+
+    @PostMapping("/save-admin")
+    public String saveAdmin(@ModelAttribute UserDtls user, @RequestParam("img") MultipartFile file, HttpSession session)
+            throws IOException {
+
+        String imageName = file.isEmpty() ? "default.jpg" : file.getOriginalFilename();
+        user.setProfileImage(imageName);
+        UserDtls saveUser = userService.saveAdmin(user);
+
+        if (!ObjectUtils.isEmpty(saveUser)) {
+            if (!file.isEmpty()) {
+                File saveFile = new ClassPathResource("static/img").getFile();
+
+                Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "profile_img" + File.separator
+                        + file.getOriginalFilename());
+
+//				System.out.println(path);
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            }
+            session.setAttribute("succMsg", "Успех");
+        } else {
+            session.setAttribute("errorMsg", "Что то не так");
+        }
+
+        return "redirect:/admin/add-admin";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "/admin/profile";
+    }
+
+    @PostMapping("/update-profile")
+    public String updateProfile(@ModelAttribute UserDtls user, @RequestParam MultipartFile img, HttpSession session) {
+        UserDtls updateUserProfile = userService.updateUserProfile(user, img);
+        if (ObjectUtils.isEmpty(updateUserProfile)) {
+            session.setAttribute("errorMsg", "Профиль не отредактирован");
+        } else {
+            session.setAttribute("succMsg", "Профиль отредактирован");
+        }
+        return "redirect:/admin/profile";
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(@RequestParam String newPassword, @RequestParam String currentPassword, Principal p,
+                                 HttpSession session) {
+        UserDtls loggedInUserDetails = commonUtil.getLoggedInUserDetails(p);
+
+        boolean matches = passwordEncoder.matches(currentPassword, loggedInUserDetails.getPassword());
+
+        if (matches) {
+            String encodePassword = passwordEncoder.encode(newPassword);
+            loggedInUserDetails.setPassword(encodePassword);
+            UserDtls updateUser = userService.updateUser(loggedInUserDetails);
+            if (ObjectUtils.isEmpty(updateUser)) {
+                session.setAttribute("errorMsg", "Пароль не отредактирован !! ошибка сервера");
+            } else {
+                session.setAttribute("succMsg", "Пароль отредактирован");
+            }
+        } else {
+            session.setAttribute("errorMsg", "Актуальный пароль неверно указан");
+        }
+
+        return "redirect:/admin/profile";
     }
 }
